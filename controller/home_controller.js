a5660ec6e72cf94cd4a5da1301605f7ce6f0d3b2
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-02-11 23:49:29 +0530

    Second Commit: controller page added and linked with route.

---
+++ controller/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function(req, res)
+{
+    return res.end("<h1>Controller page is running.</h1>")
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const homeController = require('../controller/home_controller.js');
 
 console.log('router loaded');
+
+router.get('/', homeController.home);
 
 module.exports = router;
