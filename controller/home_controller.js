b04214d9da53f596e515377bc980df3ce8d817a6
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-02-25 13:29:49 +0530

    controller and route for sign up is added.

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,4 +1,6 @@
 module.exports.home = function(req, res)
 {
+    // console.log(req.cookies);
+    // res.cookie('psm', 369);
     return res.render('home',{title: "Home"});
 }
--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,3 +1,48 @@
+const User = require('../models/userModel')
+
 module.exports.user = function(req, res){
     res.render('user',{title: "User page"});
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('signUp',{title: "User Signup page"});
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('signIn',{title: "User Signin page"});
+}
+
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.Confirm_password){
+        return res.render('signUp',{title: "User Signin page"});
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err)
+        {
+            console.log(err);
+            return;
+        }
+
+        if(!user)
+        {
+            User.create(req.body, function(err, user){
+                if(err)
+                {
+                    console.log("error while signing up.");
+                    return;
+                }
+                return res.render('signIn', {title: 'signin'});
+            })
+        }
+        else
+        {
+            return res.render('signUp',{title:'signup'})
+        }
+
+    })
+}
+
+module.exports.createSession = function(req, res){
+    // 
 }
