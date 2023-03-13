887c572eb417769964a26dc5dc13caa7f1f8a6fd
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-03-13 22:06:05 +0530

    delete functionality is added to post.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose. connect("mongodb://127.0.0.1:27017/codeial_dev123");
+mongoose. connect("mongodb://127.0.0.1:27017/codeial_dev");
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "error connecting to db"));
--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/posts');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     console.log(req.body);
@@ -9,5 +10,27 @@ module.exports.create = function(req, res){
         if(err){console.log("error while creating post"); return;}
         
         return res.redirect('back');
+    });
+}
+
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(err){
+            console.log("post could not be found whilw deleting the post");
+            return;
+        }
+
+        if(post.user == req.user.id)
+        {
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
     });
 }
