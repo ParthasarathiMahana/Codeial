89c65760915f58086e88a70d3e7ef1fd617863cc
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-04-01 16:09:35 +0530

    api creation with the delete controller funtion is done.

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,10 @@ console.log('router loaded');
 router.get('/', homeController.home);
 router.use('/user', require('./users.js'));
 router.use('/posts', require('./posts'));
-router.use('/comments', require('./comments'))
+router.use('/comments', require('./comments'));
 
 // for any further routes, access from here
 
+router.use('/api', require('./api'));
 
 module.exports = router;
